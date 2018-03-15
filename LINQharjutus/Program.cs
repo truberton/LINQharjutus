47c93f3fef4199d98d3c7640372a9f8de68569e1
Truberton <gertandry@gmail.com>
2018-03-15 22:57:29 +0200

    Initial commit

---
+++ LINQharjutus/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQharjutus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var humans1 = new List<String>
+            {
+                "Kalle",
+                "Malle",
+                "Mari",
+                "Elmar",
+                "Juku",
+            };
+
+            var SorteeritudNimed = (from nimi in humans1
+                              orderby nimi ascending
+                              select nimi).ToList();
+
+            foreach (var item in SorteeritudNimed)
+            {
+                Console.WriteLine(item);
+            }
+
+            var Pikkused = from nimi in SorteeritudNimed
+                           group nimi.Length by nimi.Length into y
+                           select y;
+
+            foreach (var item in Pikkused)
+            {
+                Console.WriteLine("{0} esineb {1} korda", item.Key, item.Count());
+            }
+            Console.ReadLine();
+        }
+    }
+}
